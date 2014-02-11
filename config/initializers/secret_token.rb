8d621df2156275603286464bd2caa6129a6c7506
luke.simpson <luke.simpson@liveality.com>
2014-02-10 19:28:33 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScaffoldDemo::Application.config.secret_key_base = '1a263ac4e32dd113e51f15176d1bc74a7698d89d2b79cd44f40186c327a44068fb2ae7367b3853514bb369c9127dd920277fcadee95e2ee9330d394fdebe74c3'
